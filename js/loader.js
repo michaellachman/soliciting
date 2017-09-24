4c6c5ddfd07ce58bb8d06f15eea20be03305f002
zibs <elizibin@gmail.com>
2017-09-23 23:54:16 -0700

    globe again

--- js/loader.js
+++ js/loader.js
@@ -2,6 +2,7 @@ $(window).on("load", function() {
     // weave your magic here.
     $(".loading").fadeTo(500, 0, function() {
       $(".pics").fadeTo(500, 1)
+      $(this).remove()
     })
 
 });
