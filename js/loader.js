8c4897873f38045a0daab88ba6e6056760047234
zibs <elizibin@gmail.com>
2017-09-24 00:05:56 -0700

    global working

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,8 @@
 $(window).on("load", function() {
     // weave your magic here.
     $(".loading").fadeTo(500, 0, function() {
+      // $(this).remove()
       $(".pics").fadeTo(500, 1)
-      $(this).remove()
     })
 
 });
